9013e84451e858d41044715847b017db111887f9
camille villard <hello@camillevillard.com>
2018-11-06 11:43:18 -0500

    added #index, #show and #create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #1. add a route to list all the tasks
+  get '/tasks/new', to: 'tasks#new'
+  post '/tasks', to: 'tasks#create', as: :task_create
+
+  get '/tasks', to: 'tasks#index', as: :tasks
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
 end
