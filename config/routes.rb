0eb1cda6e0315ec5ac913a37ff04c66a61746602
camille villard <hello@camillevillard.com>
2018-11-06 11:51:37 -0500

    #edit and #update methods, with their routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index', as: :tasks
   get '/tasks/:id', to: 'tasks#show', as: :task
 
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  patch '/tasks/:id', to: 'tasks#update'
+
 end
